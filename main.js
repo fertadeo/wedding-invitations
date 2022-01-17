3a33404e5bf01359b56310280ecea345dd951db8
Fernando Tadeo <fernandotadeos@gmail.com>
2022-01-17 15:15:38 -0300

    Interactions countdown, modal and new section styles

---
+++ main.js
@@ -0,0 +1,50 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const weddingDate = new Date(`April 23 ${currentYear } 18:00:00`);
+
+function updateCountdownTime() {
+  const currentTime = new Date();
+  const diff = weddingDate - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24 );
+  const h = Math.floor(diff / 1000 / 60 / 60 ) % 24; 
+  const m = Math.floor(diff / 1000 / 60 ) % 60; 
+  const s = Math.floor(diff / 1000 ) % 60; 
+
+  days.innerHTML = d; 
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s; 
+}
+
+// updateCountdownTime();
+setInterval(updateCountdownTime, 1000)
+
+
+
+
+// Modal
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
